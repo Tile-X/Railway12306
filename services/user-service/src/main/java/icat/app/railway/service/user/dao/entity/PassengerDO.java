7085c7ea2c6c174b27835313c42faba0975932e5
Tile-X <ghh20030403@gmail.com>
2024-06-17 19:04:54 +0800

    添加乘车人相关实体与持久层

---
+++ services/user-service/src/main/java/icat/app/railway/service/user/dao/entity/PassengerDO.java
@@ -0,0 +1,66 @@
+package icat.app.railway.service.user.dao.entity;
+
+import com.baomidou.mybatisplus.annotation.TableName;
+import icat.app.railway.framework.database.base.BaseDO;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+/**
+ * 乘车人表实体对象
+ */
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@TableName("t_passenger")
+public class PassengerDO extends BaseDO {
+
+    /**
+     * id
+     */
+    private Long id;
+
+    /**
+     * 用户名
+     */
+    private String username;
+
+    /**
+     * 真实姓名
+     */
+    private String realName;
+
+    /**
+     * 证件类型
+     */
+    private Integer idType;
+
+    /**
+     * 证件号码
+     */
+    private String idCard;
+
+    /**
+     * 优惠类型
+     */
+    private Integer discountType;
+
+    /**
+     * 手机号
+     */
+    private String phone;
+
+    /**
+     * 添加日期
+     */
+    private Date createDate;
+
+    /**
+     * 审核状态
+     */
+    private Integer verifyStatus;
+}
---
+++ services/user-service/src/main/java/icat/app/railway/service/user/dao/mapper/PassengerMapper.java
@@ -0,0 +1,7 @@
+package icat.app.railway.service.user.dao.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import icat.app.railway.service.user.dao.entity.PassengerDO;
+
+public interface PassengerMapper extends BaseMapper<PassengerDO> {
+}
